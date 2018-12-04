0ec15cc6a94e6b0b7219471e17fcad122ee2542c
Justin Joseph <justin.joseph72@outlook.com>
2018-12-04 19:24:17 +0000

    adding react router dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import dcComics from './img/DC.png';
+import marvelComics from './img/Marvel.png';
+
+function Home(){
+  return (
+    <div>
+      <h1>Welcome to the Heroes App!!!</h1>
+      <div className='container'>
+        <div className='column'>
+          <img src={dcComics} alt="this is DC logo"/>
+        </div>
+        <div className='column'>
+          <img src={marvelComics} alt="this is Marvel logo"/>
+        </div>
+      </div>
+      </div>
+
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <Home/>
       </div>
     );
   }
